a0f30240d69edced60903a287ec06dc4b067f5ee
Łukasz Brzózko <lukasz9099@gmail.com>
2023-05-01 21:04:05 +0200

    feat: render converted times

--- timesheet.user.js
+++ timesheet.user.js
@@ -13,21 +13,58 @@
 (function () {
   "use strict";
 
-  const myCss = GM_getResourceText("styles");
-  const styles = document.createElement("style");
-  styles.textContent = myCss;
-
-  document.body.prepend(styles);
-
   const SELECTORS = {
-    dashboardContent: "#dashboard-content",
+    cellWithValue: "td.nav.border.workedDay",
+  };
+
+  const IDS = {
+    dashboardContent: "dashboard-content",
+    layout: "my-layout",
   };
 
   const baseURL =
-    "https://jira.nd0.pl/rest/timesheet-gadget/1.0/timesheet.json?isGadget=true&baseUrl=https%3A%2F%2Fjira.nd0.pl&gadgetTitle=&startDate=&targetUser=&targetGroup=&collapseFieldGroups=false&excludeTargetGroup=&numOfWeeks=1&reportingDay=&projectOrFilter=&projectid=&filterid=&projectRoleId=&commentfirstword=&weekends=true&showDetails=true&sumSubTasks=false&showEmptyRows=false&groupByField=&moreFields=&offset=-1&page=1&monthView=false&sum=&sortBy=&sortDir=ASC&_=";
+    "https://jira.nd0.pl/rest/timesheet-gadget/1.0/timesheet.json?isGadget=true&baseUrl=https%3A%2F%2Fjira.nd0.pl&gadgetTitle=&startDate=&targetUser=&targetGroup=&collapseFieldGroups=false&excludeTargetGroup=&numOfWeeks=1&reportingDay=&projectOrFilter=&projectid=&filterid=&projectRoleId=&commentfirstword=&weekends=true&showDetails=true&sumSubTasks=false&showEmptyRows=false&groupByField=&moreFields=&offset=-2&page=1&monthView=false&sum=&sortBy=&sortDir=ASC&_=";
   const now = new Date();
 
   let controller;
+
+  const linkStyles = async () => {
+    const myCss = GM_getResourceText("styles");
+    const styleTag = document.createElement("style");
+    styleTag.textContent = myCss;
+
+    document.body.prepend(styleTag);
+  };
+
+  const renderLayout = ({ layoutEl, html }) => {
+    layoutEl.innerHTML = `
+  <div class="layout layout-a">
+    <div class="my-gadget gadget color1" style="position:relative">
+      ${html}
+    </div>
+  </div>
+  `;
+
+    const cells = layoutEl.querySelectorAll(SELECTORS.cellWithValue);
+    cells.forEach((cell, index) => {
+      const [firstChild] = cell.childNodes;
+      const cellContent = firstChild.textContent.trim();
+      if (!cellContent) return;
+
+      const cellNumber = cellContent.slice(0, -1);
+      const separatorIndex = cellNumber.indexOf(".");
+      const hours =
+        separatorIndex !== -1
+          ? cellNumber.slice(0, separatorIndex)
+          : cellNumber;
+
+      const fraction =
+        separatorIndex !== -1 ? `0.${cellNumber.slice(separatorIndex + 1)}` : 0;
+      const minutes = Math.round(parseFloat(fraction) * 60);
+
+      cell.textContent = `${hours}h ${minutes}m`;
+    });
+  };
 
   const fetchData = async () => {
     controller?.abort();
@@ -36,20 +73,15 @@
 
     const response = await fetch(`${baseURL}${now.getTime()}`, { signal });
     const { html } = await response.json();
-    // console.log(html);
 
-    const dashboardContentEl = document.querySelector(
-      SELECTORS.dashboardContent
-    );
+    const dashboardContentEl = document.getElementById(IDS.dashboardContent);
+    let layoutEl = document.getElementById(IDS.layout);
 
-    const layoutEl = document.createElement("div");
-    layoutEl.innerHTML = `
-    <div class="layout layout-a">
-      <div class="my-gadget gadget color1" style="position:relative">
-        ${html}
-      </div>
-    </div>
-    `;
+    if (layoutEl !== null) return renderLayout({ layoutEl, html });
+
+    layoutEl = document.createElement("div");
+    layoutEl.id = IDS.layout;
+    renderLayout({ layoutEl, html });
 
     dashboardContentEl.appendChild(layoutEl);
   };
@@ -64,5 +96,10 @@
     formatterBtn.addEventListener("click", fetchData);
   };
 
-  generateBtn();
+  const init = () => {
+    linkStyles();
+    generateBtn();
+  };
+
+  init();
 })();
