84aaaa36606a2c299e8a49b422891d70f8fa2f48
Łukasz Brzózko <lukasz9099@gmail.com>
2024-10-25 18:13:39 +0200

    feat: add a basic logic

--- src/index.user.js
+++ src/index.user.js
@@ -14,7 +14,80 @@
 (function () {
   "use strict";
 
-  const init = () => {};
+  const IDS = {
+    container: "container",
+  };
+
+  const MESSAGES = {
+    containerFound: `Znaleziono formularz ${IDS.container}`,
+    error: {
+      basic: "Error",
+      containerNotFound: `Nie znaleziono kontenera ${IDS.container}. Skrypt został wstrzymany.`,
+    },
+  };
+
+  let container;
+
+  const linkStyles = async () => {
+    const styleTag = document.createElement("style");
+    styleTag.textContent = `
+    body {
+    
+    }
+    `;
+
+    document.body.prepend(styleTag);
+  };
+
+  const handleContainerNotFound = () => {
+    window.console.error(
+      `%c ${MESSAGES.error.containerNotFound}`,
+      "background: red; color: #fff; font-size: 20px"
+    );
+  };
+
+  const lookForAppContainer = async () => {
+    const DOMElements = await new Promise((resolve, reject) => {
+      const maxAttempts = 50;
+      let attempt = 0;
+
+      const setIntervalId = setInterval(() => {
+        container = document.getElementById(IDS.container);
+        if (container) {
+          clearInterval(setIntervalId);
+          window.console.info(
+            `%c ${MESSAGES.containerFound}`,
+            "background: #B7E1CD; color: #000; font-size: 20px"
+          );
+          resolve({ container });
+        } else {
+          if (attempt >= maxAttempts) {
+            clearInterval(setIntervalId);
+            reject({ error: MESSAGES.error.containerNotFound });
+          } else {
+            attempt++;
+          }
+        }
+      }, 300);
+    });
+
+    return DOMElements;
+  };
+
+  const renderUiElements = () => {
+    console.log("renderUiElements");
+  };
+
+  const init = async () => {
+    try {
+      await lookForAppContainer();
+    } catch (err) {
+      return handleContainerNotFound();
+    }
+
+    linkStyles();
+    renderUiElements();
+  };
 
   init();
 })();
